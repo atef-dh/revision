6cb22c3b288b95c0269597d9403ad05e00e97bc8
atef-dh <atef.dwb@gmail.com>
2023-12-16 11:45:28 +0100

    done

---
+++ index.js
@@ -0,0 +1,40 @@
+function iterateAndLog(number) {
+    for (var i = 0; i<=number; i++) {
+      if (number%2===0){
+         console.log( i +"is even")
+      }
+     else  console.log(i + " is odd"); 
+}
+     
+    }
+
+    function iterateAndLog(number) {
+        while( i<=number) {
+         var i =0
+         i++
+         if (number%2===0){
+            console.log( i +"is even")
+         }else  console.log(i + " is odd"); 
+        }
+}
+// 2222222222222222222222222222222222222
+    function reverseIterateAndLogWithFor(n) {
+        for(var i=n; i>n;i--){
+          if (i%2===0){
+                 console.log( n +"is even")
+              }
+             else  console.log(n + " is odd"); 
+        }
+        }
+        
+        function reverseIterateAndLogWithwhile(n) {
+            while (i>n){
+                var i=n;
+                i++
+            }
+            if (i%2===0){
+                console.log( i +"is even")
+             }
+            else  console.log(i + " is odd"); 
+       }
+            
